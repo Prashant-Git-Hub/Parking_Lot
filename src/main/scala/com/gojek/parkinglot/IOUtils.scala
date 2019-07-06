ea2cdff65fcbe529bd5842aec9b1914416e354e6
Prashantlpu <prashantchoudharylpu@gmail.com>
2019-07-06 17:33:13 +0530

    Initial commit: Basic structure and simple classes. Test don't pass.

--- src/main/scala/com/gojek/parkinglot/IOUtils.scala
+++ src/main/scala/com/gojek/parkinglot/IOUtils.scala
@@ -6,27 +6,48 @@ class IOUtils {
     val splitedCommand = inputLowerCase.split(" ")
     val command = splitedCommand(0)
     command match {
-      case "create_parking_lot" => createParkingLot(splitedCommand(1).toInt)
+      case "create_parking_lot" => provideParkingSlot(splitedCommand(1).toInt)
       case "park" => parkVehicle(splitedCommand(1),splitedCommand(2))
       case "leave" => leave(splitedCommand(1).toInt)
       case "status" => status()
+      case "slot_numbers_for_cars_with_colour" => slotForColor(splitedCommand(1))
+      case "slot_number_for_registration_number" => slotForReg(splitedCommand(1))
+      case "registration_numbers_for_cars_with_colour" => regForColor(splitedCommand(1))
+      case _ => inputErrorHandler(input)
     }
     4
   }
 
-  def createParkingLot(lotNum: Int): Unit = {
-
+  def provideParkingSlot(totalSlot: Int): Unit = {
+    ParkingSlotMaster.totalSlot = totalSlot
   }
 
   def parkVehicle(registrationNumber: String, color: String): Unit = {
 
   }
 
-  def leave(lotNum: Int): Unit = {
+  def leave(slotNum: Int): Unit = {
 
   }
 
   def status(): Unit = {
 
   }
+
+  def slotForColor(color: String): Unit = {
+
+  }
+
+  def slotForReg(registrationNumber: String): Unit = {
+
+  }
+
+  def regForColor(color: String): Unit = {
+
+  }
+
+  def inputErrorHandler(errorInput:String): Unit = {
+
+  }
+
 }
---
+++ src/main/scala/com/gojek/parkinglot/ParkingSlotMaster.scala
@@ -0,0 +1,21 @@
+package com.gojek.parkinglot
+
+object ParkingSlotMaster {
+
+  var totalSlot = 0
+
+  var takenSlots : List[Array[String]] = null
+
+  def createParkingLot(slotCount: Int): Unit = {
+
+  }
+
+  def availableSlot(): Unit = {
+
+  }
+
+  def vacentSlot(): Unit = {
+
+  }
+
+}
